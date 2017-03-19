f3007f24c3e564a598882400aeb2733b30838033
xavcz <xavier.cazalot@gmail.com>
2017-03-19 11:34:06 +0100

    fix flaky tests (to be snapshots or so next)

--- src/FetchButton.test.js
+++ src/FetchButton.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FetchButtonPure } from './FetchButton';
 
-it('the button works with default status', () => {
+it('the button works with a "registered" status', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<FetchButtonPure />, div);
+  ReactDOM.render(<FetchButtonPure statusId='init' />, div);
 });
