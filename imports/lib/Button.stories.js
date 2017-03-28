244fe63f09861603279a479f8e023ff0bbc72dba
xavcz <xavier.cazalot@gmail.com>
2017-03-28 14:22:34 +0100

    update storybook scripts, update button stories & snapshots

--- imports/lib/Button.stories.js
+++ imports/lib/Button.stories.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Button from './Button';
+import { HubotIcon } from 'react-octicons-svg';
 
 storiesOf('Lib.Button', module)
   .add('small', () => <Button>Yay</Button>)
-  .add('big', () => <Button big>Yay</Button>);
+  .add('big', () => <Button big>Yay</Button>)
+  .add('with icon', () => <Button big><HubotIcon /> 42</Button>)
+  .add('disabled', () => <Button big disabled><HubotIcon /> No way you click me</Button>);
